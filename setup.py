81b7d70387a1b215e08b89126a1146f86b8beb95
Zvi Bazak <zvibazak@gmail.com>
2020-10-05 23:28:22 +0300

    fix for PIP

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
+
 setup(
   name = 'termshape',         
   packages = ['termshape'],   
@@ -10,12 +10,12 @@ setup(
   license='MIT',
   description = 'Tremshape is a minimalistic Python packgage, that only prints basic shapes on terminal.',   
   long_description=long_description,
-  long_description_content_type="text/markdown",  author = 'Zvi Bazak',
+  long_description_content_type="text/markdown",  
+  author = 'Zvi Bazak',
   author_email = 'zvibazak@gmail.com',
   url = 'https://github.com/zvibazak/termshape',
   download_url = 'https://github.com/zvibazak/termshape/archive/v_0.0.2.tar.gz',
   keywords = ['terminal', 'shape'],
-  install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
