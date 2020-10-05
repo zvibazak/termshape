adef52b2f5c24ac45c6c0bf02a7e4e6ee8c115a6
Zvi Bazak <zvibazak@gmail.com>
2020-10-05 19:20:33 +0300

    support test

--- termshape/termshape.py
+++ termshape/termshape.py
@@ -1,6 +1,9 @@
 def plot(canvas):
+    res = ''
     for row in [' '.join(row) for row in canvas]:
-        print(row)
+        res += row + '\n'
+    print(res)
+    return res
 
 def make_shape(list_x,list_y, eqs,ch='*'):
     canvas = [[' ' for _ in list_x] for _ in list_y]
@@ -18,23 +21,23 @@ def make_shape(list_x,list_y, eqs,ch='*'):
                 if eval(eq):
                     canvas[index_y][index_x] = ch
 
-    plot(canvas)
+    return plot(canvas)
 
 def print_square(size,ch='*'):
     x = y = range(size)
     eq = ["x==0", "x=="+str(size-1), "y==0", "y=="+str(size-1)]
-    make_shape(x,y,eq,ch)
+    return make_shape(x,y,eq,ch)
     
 def print_rect(w,h,ch='*'):
     x = range(w)
     y = range(h)
     eq = ["x==0","x=="+str(w-1),"y==0","y=="+str(h-1)]
-    make_shape(x,y,eq,ch)
+    return make_shape(x,y,eq,ch)
 
 def print_tring(h,ch='*'):
     x = y = range(h)
     eq = ["x==0","x=="+str(h)+"-y-1","y==0"]
-    make_shape(x,y,eq,ch)
+    return make_shape(x,y,eq,ch)
 
 def print_circle(r,ch='*',t=0.05):
     size=r+1
@@ -42,4 +45,4 @@ def print_circle(r,ch='*',t=0.05):
 
     #TODO calc best t
     eq = ["x**2+y**2>"+str(r**2-t*(r**2)) + " and x**2+y**2<"+str(r**2+t*(r**2))]
-    make_shape(x,y,eq,ch)
+    return make_shape(x,y,eq,ch)
--- test_termshape.py
+++ test_termshape.py
@@ -2,8 +2,9 @@ import pytest
 from termshape.termshape import *
 
 string = str(print_square(5))
-assert (string=="""* * * * *
+assert (string == """* * * * *
 *       *
 *       *
 *       *
-* * * * *""")
+* * * * *
+""")
