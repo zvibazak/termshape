b9721ffe7cb0bc479f6bc8e5422a6881fb3203ec
Zvi Bazak <zvibazak@gmail.com>
2020-10-05 23:47:30 +0300

    add more tests

--- test_termshape.py
+++ test_termshape.py
@@ -1,11 +1,75 @@
 import pytest
 from termshape.termshape import *
 
-string = get_square(5)
-assert (string == """\
+def test_get_square():
+	string = get_square(5)
+	assert (string == """\
 * * * * *
 *       *
 *       *
 *       *
 * * * * *
 """)
+
+def test_get_rectangle():
+	string = get_rectangle(10, 5)
+	assert (string == """\
+* * * * * * * * * *
+*                 *
+*                 *
+*                 *
+* * * * * * * * * *
+""")
+
+def test_get_circle():
+	string = get_circle(10)
+	assert (string == """\
+                  * * * * *                
+              *               *            
+          *                       *        
+        *                           *      
+      *                               *    
+                                           
+    *                                   *  
+                                           
+  *                                       *
+  *                                       *
+  *                                       *
+  *                                       *
+  *                                       *
+                                           
+    *                                   *  
+                                           
+      *                               *    
+        *                           *      
+          *                       *        
+              *               *            
+                  * * * * *                
+                                           
+""")
+
+def test_get_triangular():
+	string = get_triangular(10)
+	assert (string == """\
+*                  
+* *                
+*   *              
+*     *            
+*       *          
+*         *        
+*           *      
+*             *    
+*               *  
+* * * * * * * * * *
+""")
+
+def test_get_rectangle_special_character():
+	string = get_rectangle(10,5,'$')
+	assert (string == """\
+$ $ $ $ $ $ $ $ $ $
+$                 $
+$                 $
+$                 $
+$ $ $ $ $ $ $ $ $ $
+""")
+
