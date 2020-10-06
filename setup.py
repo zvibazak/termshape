bedfe8face2dad24f0d249c8f393ea01afa40976
Zvi Bazak <zvibazak@gmail.com>
2020-10-06 13:11:04 +0300

    handle version in one place

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup
+import codecs
+import os.path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
   name = 'termshape',         
   packages = ['termshape'],   
-  version = '0.0.2',      
+  version = get_version("termshape/termshape.py"),      
   license='MIT',
   description = 'Tremshape is a minimalistic Python packgage, that only prints basic shapes on terminal.',   
   long_description=long_description,
